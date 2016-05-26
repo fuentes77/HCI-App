c4f4a4175d1c2af7f8a3fb318920ce0dba7024ac
fuentes77 <fuentes.edgar@protonmail.ch>
2016-05-26 10:51:41 +0200

    Adding Login as new view

--- www/js/app.js
+++ www/js/app.js
@@ -89,7 +89,17 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
           controller: 'UbungCtrl'
         }
       }
-    })  ;
+    }) 
+
+  .state('tab.login', {
+    url: '/login',
+    views: {
+      'tab-login': {
+        templateUrl: 'templates/login.html',
+        controller: 'LoginCtrl'
+      }
+    }
+  }) ;
 
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/dash');
